2a0054d5dfd19ec7425c4700aa5b47e37317b72b
devoarm <nattapongarm2017@gmail.com>
2023-10-11 14:43:35 +0700

    edit auth

--- src/controller/authNhso.controller.ts
+++ src/controller/authNhso.controller.ts
@@ -34,43 +34,42 @@ export const checkAuthByDate = async (req: Request, res: Response) => {
   const data = req.body;
 
   const sql = `SELECT
-  v.vn,
-  v.hn,
-  p.cid,
-  CONCAT(p.fname,' ',p.lname) as fullname,
-  v.pttype,
-  pt.name,
-  v.spclty,
-  spclty.name as spc_name,
-  a.CLAIM_CODE as authen_aauth,
-  aa.AuthenCode as authen_nhso,
-  aa.ServiceDate as service_date
-  FROM vn_stat v
-  LEFT JOIN patient p ON v.hn = p.hn
-  LEFT JOIN pttype pt ON v.pttype = pt.pttype
-  LEFT JOIN spclty ON v.spclty = spclty.spclty
-  LEFT JOIN authenhos a ON a.CREATED_DATE = v.vstdate AND p.cid = a.PID
-  LEFT JOIN (SELECT
-  HCode,
-  VN,
-  CID,
-  HN,Service,
-  ServiceCode,
-  ServiceDate,
-  CAST(AuthenDate AS DATE) AS AuthenDate,
-  AuthenCode
-  FROM rcmdb.authencode 
-  WHERE CAST(AuthenDate AS DATE) = CURDATE()) AS AA ON AA.AuthenDate = v.vstdate AND p.cid = AA.CID
-  
-  WHERE       
-    v.vstdate BETWEEN '${data.vstdate}' AND '${data.vstdate}'
-     ${
-       data.timeStart == "00:00:00" && data.timeEnd == "00:00:00"
-         ? ``
-         : `AND TIME(aa.ServiceDate) BETWEEN '${data.timeStart}' AND '${data.timeEnd}'`
-     } 
-    ${data.ward.spclty != "" ? ` AND v.spclty = ${data.ward.spclty}` : ""}
-    AND p.nationality = '99'`;
+	v.vn,
+	v.hn,
+	p.cid,
+	CONCAT(p.fname,' ',p.lname) as fullname,
+	v.pttype,
+	pt.name,
+	v.spclty,
+	spclty.name as spc_name,
+	a.ServiceDate as service_date,
+	a.AuthenCode as authen_code
+	FROM vn_stat v
+	LEFT JOIN patient p ON v.hn = p.hn
+	LEFT JOIN pttype pt ON v.pttype = pt.pttype
+	LEFT JOIN spclty ON v.spclty = spclty.spclty
+	LEFT JOIN (SELECT
+		HCode,
+		VN,
+		CID,
+		HN,Service,
+		ServiceCode,
+		ServiceDate,
+		CAST(AuthenDate AS DATE) AS AuthenDate,
+		AuthenCode
+	 FROM rcmdb.authencode 
+	 WHERE 
+		CAST(AuthenDate AS DATE) = "${data.vstdate}") AS a ON p.cid = a.CID
+ WHERE       
+	v.vstdate = '${data.vstdate}'
+	${
+    data.timeStart == "00:00:00" && data.timeEnd == "00:00:00"
+      ? ``
+      : `AND TIME(a.ServiceDate) BETWEEN '${data.timeStart}' AND '${data.timeEnd}'`
+  }  
+	${data.ward.spclty != "" ? ` AND v.spclty = ${data.ward.spclty}` : ""}
+	AND p.nationality = '99'`;
+  console.log(sql)
   try {
     const query = await dbHos.raw(sql);
 
